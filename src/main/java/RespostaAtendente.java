8736bdb712b4ee44f8654bff52bfeead925e57dc
Juarez Campos <42527050+JuarezCampos93@users.noreply.github.com>
2023-12-05 08:59:00 -0300

    Update RespostaAtendente.java

--- src/main/java/RespostaAtendente.java
+++ src/main/java/RespostaAtendente.java
@@ -1,10 +1,11 @@
-public class RespostaDoPromotor implements TipoResposta {
+public class RespostaAtendente implements TipoResposta {
 
-    private static RespostaDoPromotor respostaDoPromotor = new RespostaDoPromotor();
+    private static RespostaAtendente respostaAtendente = new RespostaAtendente();
 
-    private RespostaDoPromotor(){};
+    private RespostaAtendente(){};
 
-    public static RespostaDoPromotor getRespostaDoPromotor(){
-        return respostaDoPromotor;
+    public static RespostaAtendente getRespostaDoPromotor(){
+        return respostaAtendente;
     }
 }
+
